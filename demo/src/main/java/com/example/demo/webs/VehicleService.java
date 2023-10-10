8f3a3c7bf17673e56b935f721695d0c7f2cb3f71
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-10-10 09:35:57 +0530

    Update VehicleService.java

--- demo/src/main/java/com/example/demo/webs/VehicleService.java
+++ demo/src/main/java/com/example/demo/webs/VehicleService.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "vehicle_service_new")
+@Table(name = "vehicle_service_diru")
 
 
 public class VehicleService {
