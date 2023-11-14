2d3042f59d8184041b75e500f4d985a9c090cc33
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-11-14 11:40:34 +0530

    updates

--- demo/src/main/java/com/example/demo/webs/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/webs/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
